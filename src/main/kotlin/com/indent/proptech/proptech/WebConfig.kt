8e66c2ac6d97efc56f512eee4de1cbd96e4b7c79
machim <mariusachim@gmail.com>
2025-09-29 12:18:54 +0300

    extract CORS origins in config file

--- src/main/kotlin/com/indent/proptech/proptech/WebConfig.kt
+++ src/main/kotlin/com/indent/proptech/proptech/WebConfig.kt
@@ -1,15 +1,18 @@
 package com.indent.proptech.proptech;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(private val env: Environment) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
+
+        val origins: Array<String> = env.getProperty("cors.allowed-origins", Array<String>::class.java) ?: arrayOf()
+
         registry.addMapping("/**")
-            // TODO extract as external config
-            .allowedOrigins("https://d24eqtkw4p9swm.cloudfront.net")
+            .allowedOrigins(*origins)
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
